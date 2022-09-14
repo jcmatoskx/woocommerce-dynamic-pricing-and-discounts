b20488526a45da5f7689fb807a4ad7fe957b3e7a
jcmatoskx <37616081+jcmatoskx@users.noreply.github.com>
2022-09-14 12:43:02 +0100

    Update horizontal.php
    - Makes the fullwidth of the column clickable( by using the percentage column and extending it vertically), so that users can click on the corresponding column and the amount will be added to the cart automatically

--- extensions/promotion-volume-pricing-table/templates/horizontal.php
+++ extensions/promotion-volume-pricing-table/templates/horizontal.php
@@ -11,14 +11,16 @@ if (!defined('ABSPATH')) {
 
 ?>
 
-<div class="rp_wcdpd_product_page_title"><?php echo RP_WCDPD_Settings::get('promo_volume_pricing_table_title'); ?></div>
+<div class="rp_wcdpd_product_page_title"><?php echo $title; ?></div>
 
 <div class="rp_wcdpd_pricing_table">
+
+    <?php do_action('rp_wcdpd_volume_pricing_table_before_table'); ?>
 
     <table>
         <tbody>
 
-            <tr>
+            <tr class="rp_wcdpd_pricing_table_product_quantity_row">
 
                 <?php if (count($data) > 1): ?>
                     <td>&nbsp;</td>
@@ -27,10 +29,12 @@ if (!defined('ABSPATH')) {
                 <?php foreach ($data as $single): ?>
                     <?php if ($single['table_data'] !== false): ?>
                         <?php foreach ($single['table_data'] as $range): ?>
-                            <td>
+                            <td> <div class="rp_wcdpd_pricing_table_cell_div">
                                 <span class="rp_wcdpd_pricing_table_quantity <?php echo (count($data) > 1 ? 'rp_wcdpd_pricing_table_quantity_multiple' : ''); ?>" data-rp-wcdpd-from="<?php echo $range['from']; ?>">
                                     <?php echo $range['range_label']; ?>
+									
                                 </span>
+								</div>
                             </td>
                         <?php endforeach; ?>
                         <?php break; ?>
@@ -42,13 +46,44 @@ if (!defined('ABSPATH')) {
             <?php foreach ($data as $single): ?>
                 <?php if ($single['table_data'] !== false): ?>
 
-                    <tr>
+                    <tr class="rp_wcdpd_pricing_table_product_percentage_row">
 
                         <?php if (count($data) > 1): ?>
-                            <td>
+                            <td><div class="rp_wcdpd_pricing_table_cell_div">
                                 <span class="rp_wcdpd_pricing_table_product_name" data-rp-wcdpd-variation-attributes="<?php echo RP_WCDPD_Promotion_Volume_Pricing_Table::get_variation_attributes_string($single['product']); ?>">
-                                    <?php echo wc_get_formatted_variation($single['product'], true, false); ?>
+                                    <?php echo apply_filters('rp_wcdpd_volume_pricing_table_variation_attributes', wc_get_formatted_variation($single['product'], true, false), $single['product']); ?>
                                 </span>
+								</div>
+                            </td>
+                        <?php endif; ?>
+
+                        <?php foreach ($single['table_data'] as $range): ?>
+                            <td>
+                                <span class="amount rp_wcdpd_pricing_table_product_percentage" data-rp-wcdpd-from="<?php echo $range['from']; ?>" data-rp-wcdpd-variation-attributes="<?php echo RP_WCDPD_Promotion_Volume_Pricing_Table::get_variation_attributes_string($single['product']); ?>">
+                                    <?php 
+									$percentagevalue = $range['range_per'];
+									if($percentagevalue >=30){
+										echo $percentagevalue; 
+									}
+									else {echo $percentagevalue;}
+									/*echo $range['range_per'];*/ ?>
+                                </span>
+                            </td>
+                        <?php endforeach; ?>
+
+                    </tr>
+
+			
+			
+			
+			<tr class="rp_wcdpd_pricing_table_product_price_row">
+
+                        <?php if (count($data) > 1): ?>
+                            <td><div class="rp_wcdpd_pricing_table_cell_div">
+                                <span class="rp_wcdpd_pricing_table_product_name" data-rp-wcdpd-variation-attributes="<?php echo RP_WCDPD_Promotion_Volume_Pricing_Table::get_variation_attributes_string($single['product']); ?>">
+                                    <?php echo apply_filters('rp_wcdpd_volume_pricing_table_variation_attributes', wc_get_formatted_variation($single['product'], true, false), $single['product']); ?>
+                                </span>
+								</div>
                             </td>
                         <?php endif; ?>
 
@@ -62,10 +97,17 @@ if (!defined('ABSPATH')) {
 
                     </tr>
 
+			
+			
+			
+			
+			
                 <?php endif; ?>
             <?php endforeach; ?>
 
         </tbody>
     </table>
+
+    <?php do_action('rp_wcdpd_volume_pricing_table_after_table'); ?>
 
 </div>
